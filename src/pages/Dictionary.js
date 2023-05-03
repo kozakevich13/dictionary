045c95d5783f87067de7cdbac7cc1d8f2891a7d1
kozakevich13 <hanibalsmitti@gmail.com>
2023-05-03 12:45:29 +0300

    add style to Dictionary component

--- src/pages/Dictionary.js
+++ src/pages/Dictionary.js
@@ -2,6 +2,7 @@ import React from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { removeWord } from "../actions/actions";
 import { Link } from "react-router-dom";
+import "../styles/Dictionary.css"; 
 
 const Dictionary = () => {
   const dictionary = useSelector((state) => state.words);
@@ -12,23 +13,23 @@ const Dictionary = () => {
   };
 
   return (
-    <div>
-      <h1>Dictionary</h1>
-      <Link to="/add-word">Add Word</Link>
-      <ul>
+    <div className="dictionary-container"> 
+      <h1 className="dictionary-heading">Dictionary</h1>
+      <Link to="/add-word" className="add-word-link">Add Word</Link>
+      <ul className="word-list">
         {dictionary.map((wordObj) => (
-          <li key={wordObj.word}>
+          <li key={wordObj.word} className="word-item">
             {wordObj.word} - {wordObj.translation}
-            <button onClick={() => handleRemoveWord(wordObj.word)}>
+            <button onClick={() => handleRemoveWord(wordObj.word)} className="remove-word-button">
               Remove
             </button>
           </li>
         ))}
       </ul>
       {dictionary.length >= 4 ? (
-        <Link to="/check-words">Check Words</Link>
+        <Link to="/check-words" className="check-words-link">Check Words</Link>
       ) : (
-        <span>There must be at least 4 words to check</span>
+        <span className="not-enough-words-message">There must be at least 4 words to check</span>
       )}
     </div>
   );
